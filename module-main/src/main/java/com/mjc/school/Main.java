8470db3d6e87f5568a74e830a965825ca01e31da
Dmitry Golub <dmgolub@mail.ru>
2023-08-12 00:47:14 +0400

    Console interface removed

--- module-main/src/main/java/com/mjc/school/Main.java
+++ module-main/src/main/java/com/mjc/school/Main.java
@@ -2,8 +2,10 @@ package com.mjc.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.mjc.school"})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Main {
 
 	public static void main(String[] args) {
